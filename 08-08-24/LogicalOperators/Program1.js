697ecf4fc7ce2e81ebfc96b8adf6c7c7e7f37ae3
Pallavinatraj <pallavinatraj31@gmail.com>
2024-08-09 22:21:44 +0530

    Program1

---
+++ 08-08-24/LogicalOperators/Program1.js
@@ -0,0 +1,17 @@
+//https://students.masaischool.com/assignments/10682?tab=assignmentDetails
+//Problem 1: Given the year of birth, if the age is between 13 and 19 (both included) print Teenage and in between 20 and 29 (both included) print Twenties
+let age = 22;
+if((age>13)&&(age<19)){
+    console.log('Teenage');
+}
+if((age>20)&&(age<29)){
+    console.log('Twenties');
+}
+
+
+//Problem 2 : Given any character, if it is a vowel print "Vowel"
+let character = 'e';
+if(character=='a' && character=='e' && character=='i' && character=='o' && character=='u'){
+    console.log('Vowel');
+}
+
