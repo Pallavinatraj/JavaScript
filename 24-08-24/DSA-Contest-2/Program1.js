2bd19709ae367b1a67eb0e5aa6cf36380778e7d2
Pallavinatraj <pallavinatraj31@gmail.com>
2024-08-24 22:35:01 +0530

    Program

---
+++ 24-08-24/DSA-Contest-2/Program1.js
@@ -0,0 +1,13 @@
+//Breaking a record
+function breakingTheRecord(n, m) {
+    if(m>n){
+        console.log('Broken');
+    }
+    else if(m<n){
+       console.log('Not Yet');  
+    }
+    else if(m==n){
+        console.log('Wao');
+    }
+}
+breakingTheRecord(264,200);
