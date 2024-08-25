367296ce1787ee2e41e721827d5cb7179082eb61
Pallavinatraj <pallavinatraj31@gmail.com>
2024-08-25 20:31:05 +0530

    Program

---
+++ 25-08-24/Function-2/Program1.js
@@ -0,0 +1,12 @@
+//Write a function named addNumbers which takes two numbers as input and return their sum
+function addNumbers (a,b){
+    console.log(a+b);
+}
+addNumbers(270,890);
+
+
+//Use the function to calculate the sum of given three numbers
+function addNumber (a,b,c){
+    console.log(a+b+c);
+}
+addNumber(270,890,78);
