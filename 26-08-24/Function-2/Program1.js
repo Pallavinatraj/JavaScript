185cd076fec436be9fd1a6956dec2a03420dddec
Pallavinatraj <pallavinatraj31@gmail.com>
2024-08-26 20:40:25 +0530

    Inbuilt Function

---
+++ 26-08-24/Function-2/Program1.js
@@ -0,0 +1,12 @@
+//Problem 1. Write a custom function that has the same behavior of inbuilt Array Join Function
+function inbuiltArray(arr){
+       console.log(arr.join()) ;
+       console.log(arr.join('')) ;
+       console.log(arr.join("*")) ;
+}
+inbuiltArray([1,2,3,4]);
+
+
+
+
+
---
+++ 26-08-24/Function-2/Program4.js
@@ -0,0 +1,10 @@
+//Write a custom function that has the same behavior of inbuilt String Substring Function
+
+function inbuiltsubString(str){
+    
+    console.log(str.substr());
+    console.log(str.substr(2,3));
+    console.log(str.substr(2,11));
+
+}
+inbuiltsubString("Pallavinatraj");
