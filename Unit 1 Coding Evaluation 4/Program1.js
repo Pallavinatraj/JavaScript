913657dbf69a28a3f2148bd06ec75b86bc2a64a2
Pallavinatraj <pallavinatraj31@gmail.com>
2024-09-19 02:42:01 +0530

    Program

---
+++ Unit 1 Coding Evaluation 4/Program1.js
@@ -0,0 +1,11 @@
+//The Senior Citizen
+
+function isSeniorCitizen(age) {
+    if(age>=60){
+        console.log("Senior Citizen");
+    }
+    else{
+        console.log("Not Senior Citizen");
+    }
+ }
+ isSeniorCitizen(50);
